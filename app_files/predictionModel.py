45f84c66a5c22f61800b65e5e867de9ba4a17434
Wahyu Aji Sulaiman <wahyuajisulaiman@gmail.com>
2021-06-04 21:24:54 +0700

    Combine predict with response.json

--- app_files/predictionModel.py
+++ app_files/predictionModel.py
@@ -5,13 +5,12 @@ import re
 import pickle
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import load_model
 
 MODEL = load_model('../models/nlp.h5')
 TOKENIZER = pickle.load(open('../models/tokenizer.pickle','rb'))
-with open('../dataset/dataset.json') as json_data:
-  intents = json.load(json_data)
+with open('../models/response.json') as response_data:
+  responses = json.load(response_data)
 
 def string_preprocessing(text):
   text = re.sub(r'[^\w\s]', '', text)
@@ -23,5 +22,10 @@ def predictionProc(text):
   padding_type='post'
   seq = TOKENIZER.texts_to_sequences([text])
   pad = pad_sequences(seq, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-  return int(np.argmax(MODEL.predict(pad, verbose=0)))
+  pred = int(np.argmax(MODEL.predict(pad, verbose=0)))
+  response = ""
+  for key in responses:
+    if key == f'{pred}':
+      response = responses[key]
+  return response
 
