c86ef8ff8e4533c29675536918e0bea8c0c4a7b7
Umbara <gfjaru@gmail.com>
2021-06-04 20:17:50 +0700

    Fix NoneType caused by some typos in jsonify()

--- app_files/main.py
+++ app_files/main.py
@@ -22,7 +22,7 @@ def predictor():
     logging.exception("The JSON request is broken.")
     return jsonify(status='error', predict=-1, data=content)
 
-  return jsonify(status='ok', predict=pred, data=content['data'])
+  return jsonify(status='ok', predict=pred, data=content)
 
 if __name__=='__main__':
   app.run( debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)) )
