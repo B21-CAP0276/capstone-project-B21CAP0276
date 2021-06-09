f0b7ef211b2b2095cc86de2b920b32e42247bc09
Wahyu Aji Sulaiman <wahyuajisulaiman@gmail.com>
2021-06-09 19:34:33 +0700

    Creating Pre-processing Text

--- app_files/predictionModel.py
+++ app_files/predictionModel.py
@@ -6,17 +6,40 @@ import pickle
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
 MODEL = load_model('../models/nlp.h5')
 TOKENIZER = pickle.load(open('../models/tokenizer.pickle','rb'))
 with open('../models/response.json') as response_data:
   responses = json.load(response_data)
 
+def string_preprocessing(text):
+  # Delete punctuation mark and lowing the text
+  text = re.sub(r'[^\w\s]', '', text)
+  text.lower()
+
+  # create stemmer
+  factory = StemmerFactory()
+  stemmer = factory.create_stemmer()
+  # stemming process
+  text   = stemmer.stem(text)
+
+  #Removing stopwords
+  factory = StopWordRemoverFactory()
+  stopword = factory.create_stop_word_remover()
+  
+  # Kalimat
+  output = stopword.remove(text)
+
+  return output
+
 def predictionProc(text):
   max_length = 30
   trunc_type='post'
   padding_type='post'
-  seq = TOKENIZER.texts_to_sequences([text])
+  texts = string_preprocessing(text)
+  seq = TOKENIZER.texts_to_sequences([texts])
   pad = pad_sequences(seq, maxlen=max_length, padding=padding_type, truncating=trunc_type)
   pred = int(np.argmax(MODEL.predict(pad, verbose=0)))
   response = ""
